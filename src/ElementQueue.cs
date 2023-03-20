740bb03070a8863a3227306bf0b2518ce0b20b8e
melvinkj <melvinkentj@gmail.com>
2023-03-20 13:07:00 +0700

    add : BFS

---
+++ src/ElementQueue.cs
@@ -0,0 +1,17 @@
+using System.collections.Generic;
+using System;
+namespace lib
+{
+    class ElementQueue 
+    {
+        
+        private int numOfTreasureFound;
+        private Matrix listOfVisited;
+        private List<char> path;
+        private GraphNode lastGraph;
+
+
+
+    }   
+
+}
--- src/lib/BFS.cs
+++ src/lib/BFS.cs
@@ -1,6 +1,27 @@
-class BFS {         
-    static void Main(string[] args)
-    {
-        System.Console.WriteLine("BFS Algorithm!");
+using System;
+using System.Diagnostics;
+using System.collections.Generic;
+
+namespace lib
+{
+
+    class BFS {         
+        private List<GraphNode> graph;
+        private int numOfTreasures;
+        private int numOfNodesVisited;
+        private long executionTime;
+        private Queue<Stack <GraphNode>> queue;
+        
+
+
+
+        static void fungsiPembangkit(Graph graf, Queue queue)
+        {
+            if
+        }
+
+        static void 
     }
+
+
 }
